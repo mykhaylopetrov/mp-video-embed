d019944c0f04f32821913ba24eb6daf98bbecadf
Mykhaylo Petrov <mykhaylopetrov@gmail.com>
2023-05-18 16:52:29 +0300

    Added a check for the exist of PHP functions

--- mp-video-embed.php
+++ mp-video-embed.php
@@ -16,30 +16,38 @@ add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'mp-video-embed-style' );
 });
 
-function mp_video_embed_render( $text ) {
-	$text = preg_replace( '/\[mp_video\]/', '<div class="mp-video-embed">', $text );
-	$text = preg_replace( '/\[\/mp_video\]/', '</div>', $text );
-
-   return $text;
-}
-
-function mp_video_embed_tinymce_button() {
-	if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
-		add_filter( 'mce_external_plugins', 'mp_video_embed_add_tinymce_button' );
-		add_filter( 'mce_buttons_2', 'mp_video_embed_register_tinymce_button' );
+if ( ! function_exists( 'mp_video_embed_render' ) ) {
+	function mp_video_embed_render( $text ) {
+		$text = preg_replace( '/\[mp_video\]/', '<div class="mp-video-embed">', $text );
+		$text = preg_replace( '/\[\/mp_video\]/', '</div>', $text );
+	
+	   return $text;
 	}
 }
 
-function mp_video_embed_add_tinymce_button( $plugin_array ) {
-	$plugin_array['mp_video_embed_button_script'] = plugins_url( '/assets/js/mp-video-embed-button.js', __FILE__ );
-
-	return $plugin_array;
+if ( ! function_exists( 'mp_video_embed_tinymce_button' ) ) {
+	function mp_video_embed_tinymce_button() {
+		if ( current_user_can( 'edit_posts' ) && current_user_can( 'edit_pages' ) ) {
+			add_filter( 'mce_external_plugins', 'mp_video_embed_add_tinymce_button' );
+			add_filter( 'mce_buttons_2', 'mp_video_embed_register_tinymce_button' );
+		}
+	}
 }
 
-function mp_video_embed_register_tinymce_button( $buttons ) {
-    array_push( $buttons, "mp_video" );
+if ( ! function_exists( 'mp_video_embed_add_tinymce_button' ) ) {
+	function mp_video_embed_add_tinymce_button( $plugin_array ) {
+		$plugin_array['mp_video_embed_button_script'] = plugins_url( '/assets/js/mp-video-embed-button.js', __FILE__ );
+	
+		return $plugin_array;
+	}
+}
 
-    return $buttons;
+if ( ! function_exists( 'mp_video_embed_register_tinymce_button' ) ) {
+	function mp_video_embed_register_tinymce_button( $buttons ) {
+		array_push( $buttons, "mp_video" );
+	
+		return $buttons;
+	}
 }
 
 add_filter( 'the_content', 'mp_video_embed_render', 10 );
